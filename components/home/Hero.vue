7ff0c657a6b46e4945bcd30d7e7d934b803e1c90
Yasser Jamal-Aldeen <Yasser-Jamal-Al-Deen>
2024-10-18 20:47:27 +0300

    home components

---
+++ components/home/Hero.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-card>
+    <v-card-text>
+      <v-row>
+        <v-col cols="12" class="mb-4">
+          <div class="text-lg text-gray-400 mb-2">دورة صيف</div>
+          <div class="text-3xl font-semibold">الاثنين 12 / 21 / 1998</div>
+        </v-col>
+
+        <v-col cols="4">
+          <v-btn
+            variant="outlined"
+            color="secondary"
+            size="x-large"
+            class="w-full"
+          >
+            أستاذ جديد
+          </v-btn>
+        </v-col>
+
+        <v-col cols="4">
+          <v-btn variant="outlined" color="error" size="x-large" class="w-full">
+            مسابقة جديدة
+          </v-btn>
+        </v-col>
+
+        <v-col cols="4">
+          <v-btn
+            variant="outlined"
+            color="primary"
+            size="x-large"
+            class="w-full"
+            to="/students/create"
+          >
+            طالب جديد
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-card-text>
+
+    <!-- <v-card-actions>
+      <v-btn>حفظ</v-btn>
+      <v-btn varaint="outlined">إلغاء</v-btn>
+    </v-card-actions> -->
+  </v-card>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,45 @@
 <template>
-    <v-container>
-        <v-row>
-            <v-col cols="4">
-                <v-btn variant="outlined" color="primary" size="x-large" class="w-full" to="/students/create">
-                 طالب جديد
-                </v-btn>
-            </v-col>
+  <v-container>
+    <v-row>
+      <v-col cols="12" class="mt-2">
+        <home-hero />
+      </v-col>
 
-            <v-col cols="4">
-                <v-btn variant="outlined" color="secondary" size="x-large" class="w-full">
-                 أستاذ جديد
-                </v-btn>
-            </v-col>
+      <!-- <v-col cols="4">
+                <campaign-card></campaign-card>
+            </v-col> -->
+    </v-row>
 
-            <v-col cols="4">
-                <v-btn variant="outlined" color="error" size="x-large" class="w-full">
-                 مسابقة جديدة
-                </v-btn>
-            </v-col>
-        </v-row>
-    </v-container>
+    <div class="text-xl font-semibold mt-6 mb-4">قائمة الأساتذة الموجودين</div>
+
+    <v-row>
+      <v-col cols="4">
+        <teacher-card></teacher-card>
+      </v-col>
+
+      <v-col cols="4">
+        <teacher-card></teacher-card>
+      </v-col>
+
+      <v-col cols="4">
+        <teacher-card></teacher-card>
+      </v-col>
+    </v-row>
+
+    <div class="text-xl font-semibold mt-6 mb-4">قائمة الحلقات</div>
+
+    <v-row>
+      <v-col cols="4">
+        <halakat-inline-card />
+      </v-col>
+
+      <v-col cols="4">
+        <halakat-inline-card />
+      </v-col>
+
+      <v-col cols="4">
+        <halakat-inline-card />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
