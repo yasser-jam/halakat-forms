ff1513074d3fe023a40962d973ee695a7b1d5f2d
yasserjamalaldeen <yasserjamalaldeen@gmail.com>
2025-03-12 23:33:16 +0300

    add group settings page

---
+++ pages/settings/group.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="text-xl font-semibold mb-6">إعدادات التسجيل</div>
+</template>
+
+<script setup lang="ts">
+const campaignStore = useCampaignStore();
+
+const { campaign } = storeToRefs(campaignStore);
+</script>
