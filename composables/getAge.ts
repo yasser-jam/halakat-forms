5731c24be7827b695c62c00a4f9257566c953176
Yasser-Jamal-Al-Deen <yasserjamalaldeen@gmail.com>
2025-07-10 20:13:24 +0300

    get age util

---
+++ composables/getAge.ts
@@ -0,0 +1,10 @@
+export function getAge(dateString: string): number {
+  const birthDate = new Date(dateString);
+  const today = new Date();
+  let age = today.getFullYear() - birthDate.getFullYear();
+  const m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+    age--;
+  }
+  return age;
+}
