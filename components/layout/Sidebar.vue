27792a60900808563637ded6a9a24320919ba7e0
Yasser-Jamal-Al-Deen <yasserjamalaldeen@gmail.com>
2025-07-12 17:09:21 +0300

    fix sidebar

--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -2,7 +2,7 @@
   <v-list>
     <v-list-item>
       <div class="flex flex-col items-center justify-center py-8">
-        <v-avatar size="5rem"  color="white">
+        <v-avatar size="5rem" color="white">
           <v-icon size="3rem" color="primary">mdi-mosque</v-icon>
         </v-avatar>
 
@@ -20,58 +20,73 @@
         :prepend-icon="item.icon"
         :title="item.title"
         :to="item.link"
+        :active="isActive(item.key)"
       ></v-list-item>
     </v-list>
   </v-list>
-
 </template>
 
 <script setup lang="ts">
-
-const route = useRoute()
-
+const route = useRoute();
 
 const links = ref<
   {
     title: string;
     link: string;
+    key: string;
     icon: string;
   }[]
 >([
   {
-    title: "الرئيسية",
-    link: `/dashboard`,
-    icon: "mdi-mosque",
+    title: 'الرئيسية',
+    link: `/`,
+    key: 'home',
+    icon: 'mdi-mosque',
   },
   {
-    title: "إدارة الحلقات",
+    title: 'إدارة الحلقات',
     link: `/groups-management`,
-    icon: "mdi-account-multiple",
+    key: 'groups-management',
+    icon: 'mdi-account-multiple',
   },
   {
-    title: "التفقد",
+    title: 'التفقد',
     link: `/attendance`,
-    icon: "mdi-check-outline",
+    key: 'attendance',
+    icon: 'mdi-check-outline',
   },
   {
-    title: "التسميع",
+    title: 'التسميع',
     link: `/savings`,
-    icon: "mdi-book-outline",
+    key: 'savings',
+    icon: 'mdi-book-outline',
   },
   {
-    title: "الطلاب",
+    title: 'الطلاب',
     link: `/students`,
-    icon: "mdi-account-school",
+    key: 'students',
+    icon: 'mdi-account-school-outline',
   },
   {
-    title: "الأستاتذة",
+    title: 'الأستاتذة',
     link: `/teachers`,
-    icon: "mdi-human-male-board",
+    key: 'teachers',
+    icon: 'mdi-human-male-board',
   },
   {
-    title: "سياسة الدورة",
+    title: 'سياسة الدورة',
     link: `/settings/general`,
-    icon: "mdi-cog",
+    key: 'settings',
+    icon: 'mdi-cog',
   },
 ]);
+
+const isActive = (key: string) => {
+  const route = useRoute()
+
+  if (key == 'home' && route.fullPath == '/') return true
+
+  return route.fullPath.includes(key)
+}
+
 </script>
